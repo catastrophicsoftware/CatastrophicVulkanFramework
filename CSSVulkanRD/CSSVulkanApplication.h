a303f8631aef2edcaab02a6d95087b0b9e72d352
cssoftware <contactcssoftware@gmail.com>
2020-06-13 00:27:24 -0700

    refactored

--- CSSVulkanRD/CSSVulkanApplication.h
+++ CSSVulkanRD/CSSVulkanApplication.h
@@ -1,19 +1,55 @@
 #pragma once
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
 #include <iostream>
-#include <vulkan/vulkan.h>
+#include <stdexcept>
+#include <vector>
+#include <cstring>
+#include <cstdlib>
 
-class CSSVulkanApplication
-{
+VkResult CreateDebugUtilsMessengerEXT(VkInstance instance, const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo, const VkAllocationCallbacks* pAllocator, VkDebugUtilsMessengerEXT* pDebugMessenger);
+void DestroyDebugUtilsMessengerEXT(VkInstance instance, VkDebugUtilsMessengerEXT debugMessenger, const VkAllocationCallbacks* pAllocator); 
+
+class CatastrophicVulkanApplication{
 public:
-	CSSVulkanApplication();
-	~CSSVulkanApplication();
-
-	void Run();
+    void Run();
 
 private:
-	void InitializeVulkan();
-	void MainLoop();
-	void Shutdown();
+    GLFWwindow * window;
 
-	VkInstance vkInstance;
+    VkInstance instance;
+    VkDebugUtilsMessengerEXT debugMessenger;
+
+    void initWindow();
+    void initVulkan();
+    void mainLoop();
+
+
+    void cleanup();
+    void createInstance();
+
+    void populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT& createInfo);
+
+    void setupDebugMessenger();
+
+    std::vector<const char*> getRequiredExtensions();
+
+    bool checkValidationLayerSupport();
+
+    static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity, VkDebugUtilsMessageTypeFlagsEXT messageType, const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData, void* pUserData);
+
+#ifdef NDEBUG
+    const bool enableValidationLayers = false;
+#else
+    const bool enableValidationLayers = true;
+#endif
+
+    uint32_t WindowWidth = 1280;
+    uint32_t WindowHeight = 1024;
+
+
+    const std::vector<const char*> validationLayers = {
+        "VK_LAYER_KHRONOS_validation"
+    };
 };
--- CSSVulkanRD/main.cpp
+++ CSSVulkanRD/main.cpp
@@ -1,34 +1,24 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
-#define GLM_FORCE_RADIANS
-#define GLM_FORCE_DEPTH_ZERO_TO_ONE
-#include <glm/vec4.hpp>
-#include <glm/mat4x4.hpp>
+#include "CSSVulkanApplication.h"
 #include <iostream>
+#include <stdexcept>
+#include <vector>
+#include <cstring>
+#include <cstdlib>
 
 
 int main() {
-    glfwInit();
+    CatastrophicVulkanApplication* pApp = new CatastrophicVulkanApplication();
 
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-    GLFWwindow* window = glfwCreateWindow(800, 600, "CSSOFTWARE VULKAN R&D", nullptr, nullptr);
-
-    uint32_t extensionCount = 0;
-    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
-
-    std::cout << extensionCount << " extensions supported\n";
-
-    glm::mat4 matrix;
-    glm::vec4 vec;
-    auto test = matrix * vec;
-
-    while (!glfwWindowShouldClose(window)) {
-        glfwPollEvents();
+    try {
+        pApp->Run();
+    }
+    catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
     }
 
-    glfwDestroyWindow(window);
-
-    glfwTerminate();
-
-    return 0;
+    return EXIT_SUCCESS;
 }
+
